5c08f5ec6ca044a9633ee47d01f029007b3740d7
Maxime Courchesne <maxime141414@gmail.com>
2023-05-03 19:25:16 -0400

    added a factor(n) function

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,4 @@
+
 # this gcd function is based on the euclidian algorithm
 def gcd(n, m):
     if n%m==0:
@@ -6,3 +7,15 @@ def gcd(n, m):
         return gcd(m, n)
     else:
         return gcd(m, n%m)
+
+# this factor function will be used to test how long it takes to factor the modulus "n", product of two large primes
+def factor(n):
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            print(f"{n} = {i} * {int(n/i)}")
+            # we found a factorization of n
+            return True
+        
+    # It is impossible to factor "n" i.e. "n" is prime
+    return False
+
